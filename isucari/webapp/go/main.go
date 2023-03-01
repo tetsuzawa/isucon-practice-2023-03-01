3eb16c3a48721c4a8e6f48d87cdc1ea075accf26
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2023-03-01 22:21:26 +0900

    categories from process cache for getCategoryByID

--- isucari/webapp/go/main.go
+++ isucari/webapp/go/main.go
@@ -13,6 +13,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strconv"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -169,6 +170,78 @@ type Category struct {
 	CategoryName       string `json:"category_name" db:"category_name"`
 	ParentCategoryName string `json:"parent_category_name,omitempty" db:"-"`
 }
+
+func initCategories() {
+	rawCategoryCsv := `1,0,ソファー
+2,1,一人掛けソファー
+3,1,二人掛けソファー
+4,1,コーナーソファー
+5,1,二段ソファー
+6,1,ソファーベッド
+10,0,家庭用チェア
+11,10,スツール
+12,10,クッションスツール
+13,10,ダイニングチェア
+14,10,リビングチェア
+15,10,カウンターチェア
+20,0,キッズチェア
+21,20,学習チェア
+22,20,ベビーソファ
+23,20,キッズハイチェア
+24,20,テーブルチェア
+30,0,オフィスチェア
+31,30,デスクチェア
+32,30,ビジネスチェア
+33,30,回転チェア
+34,30,リクライニングチェア
+35,30,投擲用椅子
+40,0,折りたたみ椅子
+41,40,パイプ椅子
+42,40,木製折りたたみ椅子
+43,40,キッチンチェア
+44,40,アウトドアチェア
+45,40,作業椅子
+50,0,ベンチ
+51,50,一人掛けベンチ
+52,50,二人掛けベンチ
+53,50,アウトドア用ベンチ
+54,50,収納付きベンチ
+55,50,背もたれ付きベンチ
+56,50,ベンチマーク
+60,0,座椅子
+61,60,和風座椅子
+62,60,高座椅子
+63,60,ゲーミング座椅子
+64,60,ロッキングチェア
+65,60,座布団
+66,60,空気椅子`
+
+	for _, row := range strings.Split(rawCategoryCsv, "\n") {
+		id := 0
+		parentId := 0
+		categoryName := ""
+		for i, col := range strings.Split(row, ",") {
+			// id
+			if i == 0 {
+				id, _ = strconv.Atoi(col)
+			} else if i == 1 {
+				parentId, _ = strconv.Atoi(col)
+			} else if i == 2 {
+				categoryName = col
+			}
+		}
+		categories[id] = Category{
+			ID:           id,
+			ParentID:     parentId,
+			CategoryName: categoryName,
+			//ParentCategoryName: "",
+		}
+	}
+	fmt.Println(categories)
+}
+
+// map[id]Category
+var categories = make(map[int]Category)
 
 type reqInitialize struct {
 	PaymentServiceURL  string `json:"payment_service_url"`
@@ -408,7 +481,9 @@ func getUserSimpleByID(q sqlx.Queryer, userID int64) (userSimple UserSimple, err
 }
 
 func getCategoryByID(q sqlx.Queryer, categoryID int) (category Category, err error) {
-	err = sqlx.Get(q, &category, "SELECT * FROM `categories` WHERE `id` = ?", categoryID)
+	//err = sqlx.Get(q, &category, "SELECT * FROM `categories` WHERE `id` = ?", categoryID)
+	category = categories[categoryID]
+
 	if category.ParentID != 0 {
 		parentCategory, err := getCategoryByID(q, category.ParentID)
 		if err != nil {
@@ -497,6 +572,8 @@ func postInitialize(w http.ResponseWriter, r *http.Request) {
 		// 実装言語を返す
 		Language: "Go",
 	}
+
+	initCategories()
 
 	w.Header().Set("Content-Type", "application/json;charset=utf-8")
 	json.NewEncoder(w).Encode(res)
